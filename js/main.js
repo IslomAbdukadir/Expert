ad403c9a97b01e04044a460979a0f8783dcf1d46
islomabduqodir <islomabduqodir@gmail.com>
2021-10-16 19:19:44 +0500

    tugadi

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-let span = document.querySelectorAll(".desc__span");
-let form = document.querySelectorAll(".desc__form");
+// let span = document.querySelectorAll(".desc__span");
+// let form = document.querySelectorAll(".desc__form");
 
-$(".desc__form").hide()
+// $(".desc__form").hide()
 
-$(".desc__span").hover(function () {
-    // over
-    $(".desc__form").show()
-}, function () {
-    // out
-    $(".desc__form").hide()
-});
-$(".desc__form").hover(function () {
-    // over
-    $(".desc__form").show()
-}, function () {
-    // out
-    $(".desc__form").hide()
-});
+// $(".desc__span").hover(function () {
+//     // over
+//     $(".desc__form").show()
+// }, function () {
+//     // out
+//     $(".desc__form").hide()
+// });
+// $(".desc__form").hover(function () {
+//     // over
+//     $(".desc__form").show()
+// }, function () {
+//     // out
+//     $(".desc__form").hide()
+// });
 
 // 
 
